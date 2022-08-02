495665102be27fa5c78341b4163bc4a2b8b8c44d
Aliaksandr Tsakunou <Aliaksandr_Tsakunou@epam.com>
2022-08-02 10:57:29 +0300

    Task9 implemented

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, DoCheck, OnChanges, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import {IUserData} from "../../types/user.interface";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  constructor(public authService: AuthService, private router: Router) {}
+export class HeaderComponent implements DoCheck {
+  private userInfo: IUserData;
+
+  public name = '';
+
+  private isUserDataLoading = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   public get isAuthenticated() {
     return this.authService.getIsAuthenticated();
   }
 
+  ngDoCheck() {
+    if (this.isAuthenticated && !this.isUserDataLoading) {
+      this.isUserDataLoading = true;
+      this.authService.getUserInfo().subscribe(res => {
+        this.userInfo = res;
+        this.name = res.name.first
+      });
+    }
+  }
+
   public logoutHandler() {
+    this.isUserDataLoading = false;
     this.authService.logout();
     this.router.navigate(['login']);
   }
